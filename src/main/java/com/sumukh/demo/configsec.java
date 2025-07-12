804102b53745af5c78e43e98942e8e777a768e9e
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-07-12 22:52:54 +0530

    Congfg

--- src/main/java/com/sumukh/demo/configsec.java
+++ src/main/java/com/sumukh/demo/configsec.java
@@ -1,9 +1,25 @@
 package com.sumukh.demo;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class configsec extends  {
+@EnableWebSecurity
+public class configsec {
 
+    @Bean
+    public SecurityFilterChain filter(HttpSecurity http) throws Exception {
+
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(adad -> adad.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build() ;
+
+    }
 }
