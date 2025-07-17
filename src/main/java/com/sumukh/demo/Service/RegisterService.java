038d4ba88b0b4db17b07d3caac552e85401ac258
Pg-Mighty <85114806+Pg-Mighty@users.noreply.github.com>
2025-07-17 21:11:58 +0530

    Fied issue

--- src/main/java/com/sumukh/demo/Service/RegisterService.java
+++ src/main/java/com/sumukh/demo/Service/RegisterService.java
@@ -1,4 +1,21 @@
 package com.sumukh.demo.Service;
 
-public class RegisterService {
+import com.sumukh.demo.Repo.repo;
+import com.sumukh.demo.model.Students;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterService
+{
+    @Autowired
+    repo repo;
+
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
+
+    public void register(Students student){
+        student.setPassword(encoder.encode(student.getPassword()));
+        repo.save(student);
+    }
 }
